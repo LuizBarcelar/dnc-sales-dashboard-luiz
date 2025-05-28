32a4d93e44dd940dd2c536618ae7755a96bc363c
LuizBarcelar <luizbarcelar2014@gmail.com>
2025-05-28 15:01:44 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint','react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+    ],
+  },
+};
