69a2dd9f26c5fed242e5c308e5baa3f851637cda
LuizBarcelar <luizbarcelar2014@gmail.com>
2025-06-07 08:20:36 -0300

    Feat: Chart Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { AvatarsList, CardComponent, CustomTable, Header } from '@/components'
 import { Container } from '@mui/material'
 import { currencyConverter } from '@/utils'
+import CustomChart from '@/components/CustomChart'
 
 function Home() {
   const mockListData = [
@@ -53,6 +54,13 @@ function Home() {
           <CustomTable
             headers={mockTableData.headers}
             rows={mockTableData.rows}
+          />
+        </CardComponent>
+        <CardComponent>
+          <CustomChart
+            labels={['Jan', 'Fev', 'Mar', 'Abr', 'Mai']}
+            data={[100.12, 2456.54, 986.32, 654.89, 754.89, 354.89]}
+            type="bar"
           />
         </CardComponent>
       </Container>
